d5a0b51a78c2356e4b0f909f8f4aac81a4984d50
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2020-05-12 02:37:22 +0530

    Problem 5: Given an integar A. Compute and return the square root of A.If A is not a perfect square, return floor(sqrt(A)). DO NOT USE SQRT FUNCTION FROM STANDARD LIBRARY

--- Bhavesh Solanki/11 May/Problem 5/Solution.cpp
+++ Bhavesh Solanki/11 May/Problem 5/Solution.cpp
@@ -15,27 +15,28 @@ int main()
 	cout << "Enter number: " << endl;
     cin >> num;
 
-	if (num == 0 || num == 1) {
-		cout << "Output: "<<num; 
-	}
+	if (num == 0 || num == 1) cout << "Output: "<<num; 
 
-	int start = 1, end = num, ans; 
-	while (start <= end) 
-	{		 
-		int mid = (start + end) / 2; 
-
-		if (mid*mid == num) 
-			return mid; 
-
-		
-		if (mid*mid < num) 
-		{ 
-			start = mid + 1; 
-			ans = mid; 
+	else
+	{
+		int start = 1, end = num, ans; 
+		while (start <= end) 
+		{		 
+			int mid = (start + end) / 2; 
+	
+			if (mid*mid == num) 
+				return mid; 
+	
+			
+			if (mid*mid < num) 
+			{ 
+				start = mid + 1; 
+				ans = mid; 
+			} 
+			else end = mid-1;		 
 		} 
-		else end = mid-1;		 
-	} 
- 
-	cout << "Output: "<<ans;
+	 
+		cout << "Output: "<<ans;
+	}
 } 
 
