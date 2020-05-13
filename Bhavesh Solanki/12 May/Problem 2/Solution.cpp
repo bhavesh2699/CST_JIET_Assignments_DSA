d9775992c90d1411b051c38b62e10dc7f30db869
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2020-05-13 09:33:07 +0530

    Problem 2: Search in a row wise and column wise sorted matrix. Given an n x n matrix and a number x, find the position of x in the matrix if it is present in it. Otherwise, print “Not Found”. In the given matrix, every row and column is sorted in increasing order. The designed algorithm should have linear time complexity.

--- Bhavesh Solanki/12 May/Problem 2/Solution.cpp
+++ Bhavesh Solanki/12 May/Problem 2/Solution.cpp
@@ -3,7 +3,7 @@ Search in a row wise and column wise sorted matrix. Given an n x n matrix
 and a number x, find the position of x in the matrix if it is present in it.
 Otherwise, print “Not Found”. In the given matrix, every row and column is
 sorted in increasing order. The designed algorithm should have linear time
-complexity.
+complexit
 
 Input : mat[][] = { {10, 20, 30, 40},{ 15, 25, 35, 45},{27, 29, 37, 48},{32, 33, 39, 50}};
 Element: 29
