07385c6cbcca7556b03bfc6f261f2dbd7d62e1e1
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2020-06-07 01:05:17 +0530

    June 6 and test paper

--- Bhavesh Solanki/6 June - Dynamic Programming/Catalan_Number/Catalan_Number.cpp
+++ Bhavesh Solanki/6 June - Dynamic Programming/Catalan_Number/Catalan_Number.cpp
@@ -1,1 +1,23 @@
-jhj
+/*
+FORMULA: (2n)!/((n+1)!*n!)
+*/
+#include<iostream>
+using namespace std;
+
+int cat_number(int n){
+ int i,j,arr[n],k;
+ arr[0] = 1;
+ for(i = 1; i < n; i++){
+ arr[i] = 0;
+ for(j = 0,k = i - 1; j < i; j++,k--)
+ arr[i] += arr[j] * arr[k];
+ }
+ return arr[n-1];
+}
+
+int main(){
+ int ans, n = 7;
+ ans = cat_number(n);
+ cout<<"catalan number N = "<<n<<" is: "<<ans;
+ return 0;
+}
